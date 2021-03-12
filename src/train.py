63f976949d5910013ec9a326632765b79185fe4f
ShivangMathur1 <shivang.mathur1@gmail.com>
2021-03-12 11:27:24 +0000

    Fix Minor Bugs

--- src/train.py
+++ src/train.py
@@ -11,7 +11,7 @@ def train_fit(device, model, dataloader, optimizer, criterion, dataset, ignore):
     
     for i, data in enumerate(tqdm(dataloader, total=int(len(dataset)/dataloader.batch_size))):
         if i in ignore:
-            print("Image Name : ", data['debug_img_name'], 'Idx : ', data[debug_idx])
+            print("Image Name : ", data['debug_img_name'], "Idx : ", data['debug_idx'])
         else:
             # Load the input features and labels from the training dataset
             target_caption = data['caption'].to(device)
