65e278d7a2a789dbe54fd7a0b364993b289d9a19
Shivang Mathur <shivang.mathur1@gmail.com>
2021-03-13 14:42:50 +0530

    Remove gradients during validation

--- src/train.py
+++ src/train.py
@@ -62,48 +62,39 @@ def validation_fit(device, model, dataloader, optimizer, criterion, dataset):
 
     val_running_loss = 0.0
     val_running_correct = 0
+    with torch.no_grad():
+        for i, data in enumerate(tqdm(dataloader, total=int(len(dataset)/dataloader.batch_size))):
+            # Load the input features and labels from the training dataset
+            target_caption = data['caption'].to(device)
 
-    for i, data in enumerate(tqdm(dataloader, total=int(len(dataset)/dataloader.batch_size))):
-        # Load the input features and labels from the training dataset
-        target_caption = data['caption'].to(device)
+            # Forward pass: Pass image data from training dataset, make predictions about class image belongs to (0-4 in this case)
+            predicted_caption = model(data)
 
-        # Reset the gradients to 0 for all learnable weight parameters
-        optimizer.zero_grad()
+            predicted_caption = predicted_caption.reshape(-1, predicted_caption.shape[2])
+            #predicted_caption = predicted_caption.argmax(1)
+            target_caption    = target_caption.reshape(-1)
 
-        # Forward pass: Pass image data from training dataset, make predictions about class image belongs to (0-4 in this case)
-        predicted_caption = model(data)
+            """
+            batch_size = 4
 
-        predicted_caption = predicted_caption.reshape(-1, predicted_caption.shape[2])
-        #predicted_caption = predicted_caption.argmax(1)
-        target_caption    = target_caption.reshape(-1)
+            target_caption = [4, 39]
+            predicted_caption = [4, 39, 8493]
 
-        """
-        batch_size = 4
+            target_caption.reshape(-1) = [4 * 39]
+            predicted_cation.reshape(-1, predicted_caption.shape[2]) = [138, 8493]
 
-        target_caption = [4, 39]
-        predicted_caption = [4, 39, 8493]
+            """
 
-        target_caption.reshape(-1) = [4 * 39]
-        predicted_cation.reshape(-1, predicted_caption.shape[2]) = [138, 8493]
+            # Define our loss function, and compute the loss
+            loss = criterion(predicted_caption, target_caption)
 
-        """
+            # Define our loss function, and compute the loss
+            #loss = criterion(predicted_caption.reshape(-1, predicted_caption.shape[2]), target_caption.reshape(-1))
 
-        # Define our loss function, and compute the loss
-        loss = criterion(predicted_caption, target_caption)
+            val_running_loss += loss.item()
+            _, preds = torch.max(predicted_caption.data, 1)
+            val_running_correct += (preds == target_caption).sum().item()
 
-        # Define our loss function, and compute the loss
-        #loss = criterion(predicted_caption.reshape(-1, predicted_caption.shape[2]), target_caption.reshape(-1))
-
-        val_running_loss += loss.item()
-        _, preds = torch.max(predicted_caption.data, 1)
-        val_running_correct += (preds == target_caption).sum().item()
-
-        # Backward pass: compute the gradients of the loss w.r.t. the model's parameters
-        loss.backward()
-
-        # Update the neural network weights
-        optimizer.step()
-        
     val_loss = val_running_loss/len(dataloader.dataset)
     val_accuracy = 100. * val_running_correct/len(dataloader.dataset)
 
