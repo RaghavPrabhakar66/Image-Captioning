8173a30396ac1cd7d9fea30c4ee8a4b861f9f515
Shivang Mathur <shivang.mathur1@gmail.com>
2021-03-25 12:05:43 +0530

    Remove Ignore

--- src/train.py
+++ src/train.py
@@ -2,7 +2,7 @@ import torch
 from tqdm import tqdm
 
 # training function
-def train_fit(device, model, dataloader, optimizer, criterion, dataset, ignore):
+def train_fit(device, model, dataloader, optimizer, criterion, dataset):
     # Switch model to training mode. This is necessary for layers like dropout, batchnorm etc which behave differently in training and evaluation mode
     model.train()
 
@@ -10,44 +10,41 @@ def train_fit(device, model, dataloader, optimizer, criterion, dataset, ignore):
     train_running_correct = 0
     
     for i, data in enumerate(tqdm(dataloader, total=int(len(dataset)/dataloader.batch_size))):
-        if i not in ignore:
-            # Load the input features and labels from the training dataset
-            target_caption = data['caption'].to(device)
+        # Load the input features and labels from the training dataset
+        target_caption = data['caption'].to(device)
 
-            # Reset the gradients to 0 for all learnable weight parameters
-            optimizer.zero_grad()
+        # Reset the gradients to 0 for all learnable weight parameters
+        optimizer.zero_grad()
 
-            # Forward pass: Pass image data from training dataset, make predictions about class image belongs to (0-4 in this case)
-            predicted_caption = model(data)
+        # Forward pass: Pass image data from training dataset, make predictions about class image belongs to (0-4 in this case)
+        predicted_caption = model(data)
 
-            predicted_caption = predicted_caption.reshape(-1, predicted_caption.shape[2])
-            #predicted_caption = predicted_caption.argmax(1)
-            target_caption    = target_caption.reshape(-1)
+        predicted_caption = predicted_caption.reshape(-1, predicted_caption.shape[2])
+        target_caption    = target_caption.reshape(-1)
 
-            """
-            batch_size = 4
+        """
+        batch_size = 4
 
-            target_caption = [4, 39]
-            predicted_caption = [4, 39, 8493]
+        target_caption = [4, 39]
+        predicted_caption = [4, 39, 8493]
 
-            target_caption.reshape(-1) = [4 * 39]
-            predicted_cation.reshape(-1, predicted_caption.shape[2]) = [4 * 39, 8493]
+        target_caption.reshape(-1) = [4 * 39]
+        predicted_cation.reshape(-1, predicted_caption.shape[2]) = [4 * 39, 8493]
 
-            """
+        """
 
-            # Define our loss function, and compute the loss
-            #print(predicted_caption.shape, target_caption.shape)
-            loss = criterion(predicted_caption, target_caption)
+        # Define our loss function, and compute the loss
+        loss = criterion(predicted_caption, target_caption)
 
-            train_running_loss += loss.item()
-            _, preds = torch.max(predicted_caption.data, 1)
-            train_running_correct += (preds == target_caption).sum().item()
+        train_running_loss += loss.item()
+        _, preds = torch.max(predicted_caption.data, 1)
+        train_running_correct += (preds == target_caption).sum().item()
 
-            # Backward pass: compute the gradients of the loss w.r.t. the model's parameters
-            loss.backward()
+        # Backward pass: compute the gradients of the loss w.r.t. the model's parameters
+        loss.backward()
 
-            # Update the neural network weights
-            optimizer.step()
+        # Update the neural network weights
+        optimizer.step()
 
     train_loss = train_running_loss/len(dataloader.dataset)
     train_accuracy = 100. * train_running_correct/len(dataloader.dataset)
@@ -69,7 +66,6 @@ def validation_fit(device, model, dataloader, optimizer, criterion, dataset):
             predicted_caption = model(data)
 
             predicted_caption = predicted_caption.reshape(-1, predicted_caption.shape[2])
-            #predicted_caption = predicted_caption.argmax(1)
             target_caption    = target_caption.reshape(-1)
 
             """
@@ -85,9 +81,6 @@ def validation_fit(device, model, dataloader, optimizer, criterion, dataset):
 
             # Define our loss function, and compute the loss
             loss = criterion(predicted_caption, target_caption)
-
-            # Define our loss function, and compute the loss
-            #loss = criterion(predicted_caption.reshape(-1, predicted_caption.shape[2]), target_caption.reshape(-1))
 
             val_running_loss += loss.item()
             _, preds = torch.max(predicted_caption.data, 1)
